ba7834cb6f294086a7a6124d01d7e68a725b758a
Alexis Luque <aluque@indexlabs.com.uy>
2018-04-17 14:47:16 -0300

    Logout from Auth0

--- 01-Login/src/main/java/com/auth0/example/LogoutServlet.java
+++ 01-Login/src/main/java/com/auth0/example/LogoutServlet.java
@@ -1,5 +1,6 @@
 package com.auth0.example;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,12 +11,24 @@ import java.io.IOException;
 @WebServlet(urlPatterns = {"/logout"})
 public class LogoutServlet extends HttpServlet {
 
+    private String domain;
+    private String clientId;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        domain = config.getServletContext().getInitParameter("com.auth0.domain");
+        clientId = config.getServletContext().getInitParameter("com.auth0.clientId");
+    }
+
     @Override
     protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
         if (request.getSession() != null) {
             request.getSession().invalidate();
         }
-        response.sendRedirect("/login");
+        String returnTo = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + "/portal/home";
+        String redirectLogout = String.format("https://%s/v2/logout?client_id=%s&returnTo=%s", domain, clientId, returnTo);
+        response.sendRedirect(redirectLogout);
     }
 
 }
