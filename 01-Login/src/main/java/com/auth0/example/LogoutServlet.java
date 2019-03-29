2162b90f1dea175fddbf9c369a4f9ad8a10f8f3d
Alexis Luque <aluque@indexlabs.com.uy>
2019-03-29 12:58:21 -0300

    Logout from Auth0 authorization server

--- 01-Login/src/main/java/com/auth0/example/LogoutServlet.java
+++ 01-Login/src/main/java/com/auth0/example/LogoutServlet.java
@@ -1,5 +1,6 @@
 package com.auth0.example;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,12 +11,32 @@ import java.io.IOException;
 @WebServlet(urlPatterns = {"/logout"})
 public class LogoutServlet extends HttpServlet {
 
+    private String domain;
+    private String clientId;
+
+    @Override
+    public void init(ServletConfig config) {
+        domain = config.getServletContext().getInitParameter("com.auth0.domain");
+        clientId = config.getServletContext().getInitParameter("com.auth0.clientId");
+    }
+
     @Override
     protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
         if (request.getSession() != null) {
             request.getSession().invalidate();
         }
-        response.sendRedirect("/login");
+        String returnUrl = String.format("%s://%s", request.getScheme(), request.getServerName());
+        if (request.getServerPort() != 80 && request.getServerPort() != 443) {
+            returnUrl += ":" + request.getServerPort();
+        }
+        returnUrl += "/login";
+        String logoutUrl = String.format(
+                "https://%s/v2/logout?client_id=%s&returnTo=%s",
+                domain,
+                clientId,
+                returnUrl
+        );
+        response.sendRedirect(logoutUrl);
     }
 
 }
