1a196306d9a74bc18e6a1fb198113f4f6bb222b8
Jim Anderson <jimranderson@gmail.com>
2023-10-11 19:09:12 -0500

    fix singleton

--- 01-Login/src/main/java/com/auth0/example/AuthenticationControllerProvider.java
+++ 01-Login/src/main/java/com/auth0/example/AuthenticationControllerProvider.java
@@ -22,22 +22,22 @@ class AuthenticationControllerProvider {
 
     // if multiple threads may call this, synchronize this method and consider double locking
     static AuthenticationController getInstance(ServletConfig config) throws UnsupportedEncodingException {
-        if (INSTANCE != null) {
-            return INSTANCE;
+        if (INSTANCE == null) {
+            String domain = config.getServletContext().getInitParameter("com.auth0.domain");
+            String clientId = config.getServletContext().getInitParameter("com.auth0.clientId");
+            String clientSecret = config.getServletContext().getInitParameter("com.auth0.clientSecret");
+
+            if (domain == null || clientId == null || clientSecret == null) {
+                throw new IllegalArgumentException("Missing domain, clientId, or clientSecret. Did you update src/main/webapp/WEB-INF/web.xml?");
+            }
+
+            // JwkProvider required for RS256 tokens. If using HS256, do not use.
+            JwkProvider jwkProvider = new JwkProviderBuilder(domain).build();
+            INSTANCE = AuthenticationController.newBuilder(domain, clientId, clientSecret)
+                    .withJwkProvider(jwkProvider)
+                    .build();
         }
 
-        String domain = config.getServletContext().getInitParameter("com.auth0.domain");
-        String clientId = config.getServletContext().getInitParameter("com.auth0.clientId");
-        String clientSecret = config.getServletContext().getInitParameter("com.auth0.clientSecret");
-
-        if (domain == null || clientId == null || clientSecret == null) {
-            throw new IllegalArgumentException("Missing domain, clientId, or clientSecret. Did you update src/main/webapp/WEB-INF/web.xml?");
-        }
-
-        // JwkProvider required for RS256 tokens. If using HS256, do not use.
-        JwkProvider jwkProvider = new JwkProviderBuilder(domain).build();
-        return AuthenticationController.newBuilder(domain, clientId, clientSecret)
-                .withJwkProvider(jwkProvider)
-                .build();
+        return INSTANCE;
     }
 }
